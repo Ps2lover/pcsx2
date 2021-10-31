facccd4a77e3c8c69814e99fcec56f43307decb9
Connor McLaughlin <stenzek@gmail.com>
2021-10-31 19:21:35 +1000

    3rdparty: Add cpuinfo

--- cmake/SearchForStuff.cmake
+++ cmake/SearchForStuff.cmake
@@ -276,6 +276,7 @@ add_subdirectory(3rdparty/simpleini EXCLUDE_FROM_ALL)
 add_subdirectory(3rdparty/imgui EXCLUDE_FROM_ALL)
 add_subdirectory(3rdparty/libzip EXCLUDE_FROM_ALL)
 add_subdirectory(3rdparty/bc7enc EXCLUDE_FROM_ALL)
+add_subdirectory(3rdparty/cpuinfo EXCLUDE_FROM_ALL)
 
 if(USE_OPENGL)
 	add_subdirectory(3rdparty/glad EXCLUDE_FROM_ALL)
